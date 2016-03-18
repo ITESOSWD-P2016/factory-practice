6ce9dc09e4665f804e39360b1037dd9aa928d4a1
alejandragalindo <alegalindo92@gmail.com>
2016-03-18 08:32:27 -0600

    ya termine

--- src/main/java/com/iteso/factory/JamaiconInstaller.java
+++ src/main/java/com/iteso/factory/JamaiconInstaller.java
@@ -4,4 +4,5 @@ package com.iteso.factory;
  * Created by rvillalobos on 3/17/16.
  */
 public interface JamaiconInstaller {
+
 }
--- src/main/java/com/iteso/factory/installers/JamaiconSportsWindowsInstaller.java
+++ src/main/java/com/iteso/factory/installers/JamaiconSportsWindowsInstaller.java
@@ -1,7 +1,10 @@
 package com.iteso.factory.installers;
 
+import com.iteso.factory.JamaiconInstaller;
+
 /**
  * Created by User on 18/03/2016.
  */
-public class JamaiconSportsWindowsInstaller {
+public class JamaiconSportsWindowsInstaller implements JamaiconInstaller {
+
 }
